d6f0831d613824beb69ab24ca2a1ed649f353bd8
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-03-17 15:51:27 +0100

    Modify functionality to work with hstore_translate
    Modified original code from `friendly_id-globalize` to work with
    `hstore_translates` instead of `globalize3`.
    
    Translations are now checked and saved by using helper methods of
    this translations gem.

--- lib/friendly_id/hstore.rb
+++ lib/friendly_id/hstore.rb
@@ -14,32 +14,35 @@ module FriendlyId
 
       def advise_against_untranslated_model(model)
         field = model.friendly_id_config.query_field
-        unless model.respond_to?('translated_attribute_names') ||
-               model.translated_attribute_names.exclude?(field.to_sym)
+        unless model.respond_to?('translated_attrs') ||
+               model.translated_attrs.exclude?(field.to_sym)
           raise "[FriendlyId] You need to translate the '#{field}' field with " \
-            "hstore_stranslates (add 'translates :#{field}' in your model '#{model.name}')"
+            "hstore_translates (add 'translates :#{field}' in your model '#{model.name}')"
         end
       end
+
       private :advise_against_untranslated_model
     end
 
     def set_friendly_id(text, locale = nil)
-      ::Globalize.with_locale(locale || ::Globalize.locale) do
+      execute_with_locale(locale || ::I18n.locale) do
         set_slug normalize_friendly_id(text)
       end
     end
 
     def should_generate_new_friendly_id?
-      translation_for(::Globalize.locale).send(friendly_id_config.slug_column).nil?
+      translations = get_field_translations_hash
+      return (translations.blank? || [::I18n.locale.to_s].blank?)
     end
 
     def set_slug(normalized_slug = nil)
-      if self.translations.size > 1
-        self.translations.map(&:locale).each do |locale|
-          ::Globalize.with_locale(locale) { super_set_slug(normalized_slug) }
+      translations = get_field_translations_hash
+      if !translations.blank? && translations.size > 1
+        translations.each do |locale, value|
+          execute_with_locale(locale) { super_set_slug(normalized_slug) }
         end
       else
-        ::Globalize.with_locale(::Globalize.locale) { super_set_slug(normalized_slug) }
+        execute_with_locale(::I18n.locale) { super_set_slug(normalized_slug) }
       end
     end
 
@@ -47,7 +50,28 @@ module FriendlyId
       if should_generate_new_friendly_id?
         candidates = FriendlyId::Candidates.new(self, normalized_slug || send(friendly_id_config.base))
         slug = slug_generator.generate(candidates) || resolve_friendly_id_conflict(candidates)
-        translation.slug = slug
+        self.send("#{friendly_id_config.slug_column}=", slug)
+      end
+    end
+
+    def get_field_translations_hash
+      self.send("#{friendly_id_config.slug_column}_translations")
+    end
+
+    # Auxiliar function to execute a block with other locale set
+    #
+    def execute_with_locale(locale=::I18n.locale, &block)
+      actual_locale = ::I18n.locale
+      ::I18n.locale = locale
+
+      block.call
+
+      ::I18n.locale = actual_locale
+    end
+
+    FriendlyId::FinderMethods.class_eval do
+      def exists_by_friendly_id?(id)
+        send("with_#{friendly_id_config.query_field}_translation", id).exists?
       end
     end
   end
